56d7f9ef039bdb9d9f9de768fab017cc86f0ad2e
AniketGodse <149289805+AniketGodse@users.noreply.github.com>
2023-10-29 18:52:27 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,31 @@
+first = input ("Enter first number: ")
+
+operator = input ("Enter ooperator (+,-,*,/,%) : ")
+
+second = input ("Enter second number : ")
+
+
+
+if operator == "+":
+    print(first + second)
+    
+elif operator == "-":
+    print(first - second)
+    
+elif operator == "+":
+    print(first * second)
+    
+elif operator == "*":
+    print(first * second)
+    
+elif operator == "/":
+    print(first / second)
+    
+elif operator == "%":
+    print(first % second)
+    
+else:
+    print("Invalid Operation")
+    
+    
+    
